06127ec4715fd5cb418032de4fbf9ea986823462
le trung hieu <lehieuevn112@gmail.com>
2016-03-22 16:22:10 +0700

    Merge origin/master
    Conflicts:
    	src/main/webapp/saleman/addlead.xhtml

--- src/main/java/managedBeans/LeadsManagedBean.java
+++ src/main/java/managedBeans/LeadsManagedBean.java
@@ -6,7 +6,9 @@
 package managedBeans;
 
 import ejb.LeadsFacade;
+import ejb.UsersFacade;
 import entities.Leads;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
@@ -23,8 +25,18 @@ public class LeadsManagedBean {
     
     @Inject
     private LeadsFacade leadEJB;
+    @Inject
+    private UsersFacade userEJB;
     private Leads lead= new Leads();
+    private List<Leads> searchList;
 
+    public List<Leads> getSearchList() {
+        return searchList;
+    }
+
+    public void setSearchList(List<Leads> searchList) {
+        this.searchList = searchList;
+    }
     public Leads getLead() {
         return lead;
     }
@@ -37,11 +49,16 @@ public class LeadsManagedBean {
     }
     
     public String doCreateLead(){
+        lead.setUserID(userEJB.find(1));
         leadEJB.create(lead);
         lead = new Leads();
         FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO, "Info",
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Create Success",
                 "Create Success !"));
         return "addlead.xhtml";
+    }
+    
+    public List<Leads> doFindAllLeads(){
+        return leadEJB.findAll();
     }
 }
